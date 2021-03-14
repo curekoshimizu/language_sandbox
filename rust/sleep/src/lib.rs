1daa1ad66b9951c2e34ba0d8bcaf0769547aa350
curekoshimizu <shugo@manabe.mail-box.ne.jp>
2021-03-14 20:39:05 +0900

    add args

--- rust/sleep/src/lib.rs
+++ rust/sleep/src/lib.rs
@@ -1,18 +1,18 @@
 use std::thread;
 use std::time::{Duration, Instant};
 
-pub fn elapsed_time1() -> u64 {
+pub fn elapsed_time1(secs: u64) -> u64 {
     let start = Instant::now();
 
-    thread::sleep(Duration::from_secs(1));
+    thread::sleep(Duration::from_secs(secs));
 
     start.elapsed().as_secs()
 }
 
-pub fn elapsed_time2() -> u128 {
+pub fn elapsed_time2(millisec: u64) -> u128 {
     let start = Instant::now();
 
-    thread::sleep(Duration::from_millis(100));
+    thread::sleep(Duration::from_millis(millisec));
 
     (Instant::now() - start).as_millis()
 }
@@ -23,7 +23,7 @@ mod tests {
 
     #[test]
     fn test_sleep() {
-        assert_eq!(elapsed_time1(), 1);
-        assert_eq!(elapsed_time2(), 100);
+        assert_eq!(elapsed_time1(1), 1);
+        assert_eq!(elapsed_time2(100), 100);
     }
 }
