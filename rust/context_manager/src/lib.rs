9c679892883f3ce780e19485b4277800b07987f5
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-03-25 00:17:37 +0900

    add like context_manager example

---
+++ rust/context_manager/src/lib.rs
@@ -0,0 +1,39 @@
+use std::time::Instant;
+
+#[derive(Debug)]
+pub struct Timer {
+    start: Instant,
+}
+
+impl Timer {
+    pub fn new() -> Timer {
+        Timer {
+            start: Instant::now(),
+        }
+    }
+}
+
+impl Drop for Timer {
+    fn drop(&mut self) {
+        let elapsed = self.start.elapsed().as_millis();
+        println!("elapsed time : {} msec!!!!!!", elapsed);
+        assert!(elapsed >= 100);
+    }
+}
+
+//impl Drop for Timer {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::thread;
+    use std::time::Duration;
+
+    #[test]
+    fn it_works() {
+        let t = Timer::new();
+        thread::sleep(Duration::from_millis(100));
+        println!("debug        : {:?}", t);
+        println!("pretty debug : {:#?}", t);
+    }
+}
