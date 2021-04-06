3b786c0fc8ba3f10222c04c81d21e3d488ecee79
curekoshimizu <shugo@manabe.mail-box.ne.jp>
2021-04-06 23:07:21 +0900

    use write_all

--- rust/async_file_io/src/lib.rs
+++ rust/async_file_io/src/lib.rs
@@ -23,34 +23,56 @@ mod tests {
         let tmp = TempDir::new()?;
         let temp_dir = tmp.path().to_path_buf();
 
-        let loop_cnt = 1000000;
+        let write_loop_cnt = 1000000;
+        let read_loop_cnt = 1000;
         let bytes_data = b"party parrot".to_vec();
 
+        let fname = temp_dir.join("write_small_bytes_sync");
         measure!(" sync write small bytes", {
             use std::fs::File;
             use std::io::BufWriter;
             use std::io::Write;
 
-            let fname = temp_dir.join("write_small_bytes_sync");
-            let mut f = BufWriter::new(File::create(fname)?);
-            for _ in 0..loop_cnt {
-                f.write(&bytes_data)?;
+            let mut f = BufWriter::new(File::create(&fname)?);
+            for _ in 0..write_loop_cnt {
+                f.write_all(&bytes_data)?;
             }
         });
 
+        measure!(" sync read all bytes (1)", {
+            use std::fs;
+
+            for _ in 0..read_loop_cnt {
+                let buf = fs::read(&fname)?;
+                assert_eq!(buf.len(), bytes_data.len() * write_loop_cnt);
+                assert_eq!(buf[0], b'p');
+                assert_eq!(buf.last(), Some(&b't'));
+            }
+        });
+
+        let fname = temp_dir.join("write_small_bytes_async");
         measure!("async write small bytes", {
             use tokio::fs::File;
             use tokio::io::AsyncWriteExt;
             use tokio::io::BufWriter;
 
-            let fname = temp_dir.join("write_small_bytes_async");
-            let mut f = BufWriter::new(File::create(fname).await?);
-            for _ in 0..loop_cnt {
-                f.write(&bytes_data).await?;
+            let mut f = BufWriter::new(File::create(&fname).await?);
+            for _ in 0..write_loop_cnt {
+                f.write_all(&bytes_data).await?;
             }
+            f.flush().await?; // If it is removed, test won't pass...
         });
 
-        // TODO: add assert
+        measure!("async read all bytes (1)", {
+            use tokio::fs;
+
+            for _ in 0..read_loop_cnt {
+                let buf = fs::read(&fname).await?;
+                assert_eq!(buf.len(), bytes_data.len() * write_loop_cnt);
+                assert_eq!(buf[0], b'p');
+                assert_eq!(buf.last(), Some(&b't'));
+            }
+        });
 
         Ok(())
     }
@@ -60,34 +82,56 @@ mod tests {
         let tmp = TempDir::new()?;
         let temp_dir = tmp.path().to_path_buf();
 
-        let loop_cnt = 10;
+        let write_loop_cnt = 15;
+        let read_loop_cnt = 1;
         let bytes_data: Vec<u8> = vec![0xB; 100_000_000]; // all B array
 
+        let fname = temp_dir.join("write_big_bytes_sync");
         measure!(" sync write big bytes", {
             use std::fs::File;
             use std::io::BufWriter;
             use std::io::Write;
 
-            let fname = temp_dir.join("write_big_bytes_sync");
-            let mut f = BufWriter::new(File::create(fname)?);
-            for _ in 0..loop_cnt {
-                f.write(&bytes_data)?;
+            let mut f = BufWriter::new(File::create(&fname)?);
+            for _ in 0..write_loop_cnt {
+                f.write_all(&bytes_data)?;
             }
         });
 
+        measure!(" sync read all bytes (2)", {
+            use std::fs;
+
+            for _ in 0..read_loop_cnt {
+                let buf = fs::read(&fname)?;
+                assert_eq!(buf.len(), bytes_data.len() * write_loop_cnt);
+                assert_eq!(buf[0], 0xB);
+                assert_eq!(buf.last(), Some(&0xB));
+            }
+        });
+
+        let fname = temp_dir.join("write_big_bytes_async");
         measure!("async write big bytes", {
             use tokio::fs::File;
             use tokio::io::AsyncWriteExt;
             use tokio::io::BufWriter;
 
-            let fname = temp_dir.join("write_big_bytes_async");
-            let mut f = BufWriter::new(File::create(fname).await?);
-            for _ in 0..loop_cnt {
-                f.write(&bytes_data).await?;
+            let mut f = BufWriter::new(File::create(&fname).await?);
+            for _ in 0..write_loop_cnt {
+                f.write_all(&bytes_data).await?;
             }
+            f.flush().await?; // If it is removed, test won't pass...
         });
 
-        // TODO: add assert
+        measure!("async read all bytes (2)", {
+            use tokio::fs;
+
+            for _ in 0..read_loop_cnt {
+                let buf = fs::read(&fname).await?;
+                assert_eq!(buf.len(), bytes_data.len() * write_loop_cnt);
+                assert_eq!(buf[0], 0xB);
+                assert_eq!(buf.last(), Some(&0xB));
+            }
+        });
 
         Ok(())
     }
@@ -97,7 +141,7 @@ mod tests {
         let tmp = TempDir::new()?;
         let temp_dir = tmp.path().to_path_buf();
 
-        let loop_cnt = 100000;
+        let write_loop_cnt = 1000000;
         let str_data = "party parrot".to_string();
 
         measure!(" sync write small str (use macro)", {
@@ -107,7 +151,7 @@ mod tests {
 
             let fname = temp_dir.join("write_small_str_sync_use_macro");
             let mut f = BufWriter::new(File::create(fname)?);
-            for _ in 0..loop_cnt {
+            for _ in 0..write_loop_cnt {
                 write!(f, "{}", &str_data)?;
             }
         });
@@ -119,8 +163,8 @@ mod tests {
 
             let fname = temp_dir.join("write_small_str_sync_use_write");
             let mut f = BufWriter::new(File::create(fname)?);
-            for _ in 0..loop_cnt {
-                f.write(str_data.as_bytes())?;
+            for _ in 0..write_loop_cnt {
+                f.write_all(str_data.as_bytes())?;
             }
         });
 
@@ -131,8 +175,8 @@ mod tests {
 
             let fname = temp_dir.join("write_small_str_async");
             let mut f = BufWriter::new(File::create(fname).await?);
-            for _ in 0..loop_cnt {
-                f.write(str_data.as_bytes()).await?;
+            for _ in 0..write_loop_cnt {
+                f.write_all(str_data.as_bytes()).await?;
             }
         });
 
@@ -144,7 +188,7 @@ mod tests {
         let tmp = TempDir::new()?;
         let temp_dir = tmp.path().to_path_buf();
 
-        let loop_cnt = 10;
+        let write_loop_cnt = 10;
         let str_data = "a".repeat(10_000_000);
 
         measure!(" sync write big str (use macro)", {
@@ -154,7 +198,7 @@ mod tests {
 
             let fname = temp_dir.join("write_big_str_sync_use_macro");
             let mut f = BufWriter::new(File::create(fname)?);
-            for _ in 0..loop_cnt {
+            for _ in 0..write_loop_cnt {
                 write!(f, "{}", &str_data)?;
             }
         });
@@ -166,8 +210,8 @@ mod tests {
 
             let fname = temp_dir.join("write_big_str_sync_use_write");
             let mut f = BufWriter::new(File::create(fname)?);
-            for _ in 0..loop_cnt {
-                f.write(str_data.as_bytes())?;
+            for _ in 0..write_loop_cnt {
+                f.write_all(str_data.as_bytes())?;
             }
         });
 
@@ -178,8 +222,42 @@ mod tests {
 
             let fname = temp_dir.join("write_big_str_async");
             let mut f = BufWriter::new(File::create(fname).await?);
-            for _ in 0..loop_cnt {
-                f.write(str_data.as_bytes()).await?;
+            for _ in 0..write_loop_cnt {
+                f.write_all(str_data.as_bytes()).await?;
+            }
+        });
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn read_all_bytes() -> Result<(), Error> {
+        let tmp = TempDir::new()?;
+        let temp_dir = tmp.path().to_path_buf();
+
+        let write_loop_cnt = 20000;
+        let bytes_data = b"party parrot".to_vec();
+
+        let fname = temp_dir.join("data");
+        {
+            use std::fs::File;
+            use std::io::BufWriter;
+            use std::io::Write;
+
+            let mut f = BufWriter::new(File::create(&fname)?);
+            for _ in 0..write_loop_cnt {
+                f.write_all(&bytes_data)?;
+            }
+        }
+
+        measure!(" sync read all bytes", {
+            use std::fs;
+
+            for _ in 0..write_loop_cnt {
+                let buf = fs::read(&fname)?;
+                assert_eq!(buf.len(), bytes_data.len() * write_loop_cnt);
+                assert_eq!(buf[0], b'p');
+                assert_eq!(buf.last(), Some(&b't'));
             }
         });
 
