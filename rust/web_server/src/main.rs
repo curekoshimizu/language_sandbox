0b9dc2cba6b337b0435dff08d43e88dc880963bb
curekoshimizu <shugo@manabe.mail-box.ne.jp>
2021-04-06 23:07:35 +0900

    add httpmock test and reqwest example

--- rust/web_server/src/main.rs
+++ rust/web_server/src/main.rs
@@ -16,3 +16,44 @@ async fn main() -> std::io::Result<()> {
     info!("server is running");
     server.run().await
 }
+
+#[cfg(test)]
+mod tests {
+
+    #[tokio::test]
+    async fn test_hoge() -> Result<(), reqwest::Error> {
+        use serde_json::{json, Value};
+
+        use httpmock::Method::POST;
+        use httpmock::MockServer;
+
+        // Arrange
+        let server = MockServer::start_async().await;
+
+        let m = server.mock(|when, then| {
+            when.method(POST)
+                .path("/users")
+                .header("Content-Type", "application/json")
+                .json_body(json!({ "name": "hoge" }));
+            then.status(201)
+                .header("Content-Type", "application/json")
+                .json_body(json!({ "id": 1234 }));
+        });
+
+        let client = reqwest::Client::new();
+
+        let resp = client
+            .post(&format!("http://{}/users", server.address()))
+            .json(&json![{"name": "hoge"}])
+            .send()
+            .await?;
+
+        assert_eq!(resp.status(), 201);
+        let ret: Value = resp.json().await?;
+        assert_eq!(ret, json!({ "id": 1234} ));
+
+        m.assert();
+
+        Ok(())
+    }
+}
