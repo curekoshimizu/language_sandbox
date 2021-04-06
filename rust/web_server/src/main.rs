2a1724191b0cf905b4a755b16508d77d46d2f699
curekoshimizu <shugo@manabe.mail-box.ne.jp>
2021-04-06 23:07:31 +0900

    add webserver example

---
+++ rust/web_server/src/main.rs
@@ -0,0 +1,22 @@
+use actix_web::{App, HttpServer};
+use env_logger;
+use std::env;
+
+#[macro_use]
+extern crate log;
+
+mod config;
+
+// TODO: template
+// TODO: test code
+// TODO: not allowed
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env::set_var("RUST_LOG", "debug");
+    env_logger::init();
+
+    let server = HttpServer::new(|| App::new().configure(config::config)).bind("127.0.0.1:8080")?;
+    info!("server is running");
+    server.run().await
+}
