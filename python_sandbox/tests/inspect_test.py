169a5a6e53aa2efba4178dbb566f2426bb75b22d
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-04-06 23:07:01 +0900

    add typing

--- python_sandbox/tests/inspect_test.py
+++ python_sandbox/tests/inspect_test.py
@@ -1,6 +1,7 @@
-import inspect
 import functools
+import inspect
 import time
+from typing import Any, Callable
 
 
 class A:
@@ -10,22 +11,21 @@ class A:
 
 
 class B(A):
-    def hello(self) -> str:
-        ret = super().hello(0.01) + " world B!"
+    def hello(self, dt: float) -> str:
+        ret = super().hello(dt) + " world B!"
         time.sleep(0.02)
         return ret
 
 
 class C(A):
-    def hello(self) -> str:
-        return super().hello(0.05) + " world C!"
+    def hello(self, dt: float) -> str:
+        return super().hello(dt) + " world C!"
 
 
-
-def wrap_class_method(cls):
-    def decorate_func(class_name, f):
+def wrap_class_method(cls: Any) -> None:
+    def decorate_func(class_name: str, f: Callable[..., Any]) -> Any:
         @functools.wraps(f)
-        def wrapper(*args, **kwargs):
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
             stime = time.time()
             ret = f(*args, **kwargs)
             print(f"{class_name}.{f.__name__} : elapsed time = {(time.time() - stime):.3f} sec")
@@ -38,7 +38,7 @@ def wrap_class_method(cls):
         setattr(cls, name, decorate_func(cls.__name__, method))
 
 
-def test_class_method_hook():
+def test_class_method_hook() -> None:
     wrap_class_method(A)
     wrap_class_method(B)
     wrap_class_method(C)
@@ -46,5 +46,5 @@ def test_class_method_hook():
     b = B()
     c = C()
 
-    print(b.hello())
-    print(c.hello())
+    print(b.hello(0.01))
+    print(c.hello(0.05))
