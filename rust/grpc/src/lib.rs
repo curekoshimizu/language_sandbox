3575c5e1d462aa5776e0765436e1bdb47fb4fb53
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-04-07 20:28:36 +0900

    add stream

---
+++ rust/grpc/src/lib.rs
@@ -0,0 +1,95 @@
+use tokio::sync::mpsc;
+use tokio_stream::wrappers::ReceiverStream;
+use tonic::{Request, Response, Status};
+
+pub mod foo {
+    tonic::include_proto!("foo");
+}
+
+use foo::foo_service_server::FooService;
+use foo::{RequestMsg, ResponseMsg};
+
+#[derive(Default)]
+pub struct FooServer {}
+
+#[tonic::async_trait]
+impl FooService for FooServer {
+    async fn foo(&self, request: Request<RequestMsg>) -> Result<Response<ResponseMsg>, Status> {
+        let reply = ResponseMsg {
+            response: format!("{} hello", request.get_ref().prefix),
+        };
+
+        Ok(Response::new(reply))
+    }
+
+    type FooStreamStream = ReceiverStream<Result<ResponseMsg, Status>>;
+
+    async fn foo_stream(
+        &self,
+        request: Request<RequestMsg>,
+    ) -> Result<Response<Self::FooStreamStream>, Status> {
+        let (tx, rx) = mpsc::channel(2);
+
+        let recv_stream = ReceiverStream::new(rx);
+
+        let reply = ResponseMsg {
+            response: format!("{} hello", request.get_ref().prefix),
+        };
+        tx.send(Ok(reply.clone())).await.unwrap();
+        tx.send(Ok(reply)).await.unwrap();
+
+        Ok(Response::new(recv_stream))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use foo::foo_service_client::FooServiceClient;
+    use foo::foo_service_server::FooServiceServer;
+    use tokio::sync::oneshot;
+    use tonic::transport::Server;
+
+    #[tokio::test]
+    async fn grpc_foo_test() -> Result<(), Box<dyn std::error::Error>> {
+        let address = "127.0.0.1:50051";
+        let addr = address.parse().unwrap();
+
+        use futures_util::FutureExt;
+
+        let (shutdown_tx, shutdown_rx) = oneshot::channel::<()>();
+
+        let s = FooServer::default();
+        let server = Server::builder().add_service(FooServiceServer::new(s));
+        let (ready_tx, ready_rx) = oneshot::channel();
+
+        let server_future = tokio::spawn(async move {
+            ready_tx.send(()).unwrap(); // I am ready.
+
+            server
+                .serve_with_shutdown(addr, shutdown_rx.map(|rx| drop(rx)))
+                .await
+        });
+
+        let received = ready_rx.await?;
+        assert_eq!(received, ());
+
+        {
+            let mut client = FooServiceClient::connect(format!("http://{}", address)).await?;
+
+            let request = tonic::Request::new(RequestMsg {
+                prefix: "`from client`".to_string(),
+            });
+
+            let response = client.foo(request).await?;
+
+            assert_eq!(response.get_ref().response, "`from client` hello");
+        }
+
+        shutdown_tx.send(()).unwrap();
+
+        server_future.await??;
+
+        Ok(())
+    }
+}
