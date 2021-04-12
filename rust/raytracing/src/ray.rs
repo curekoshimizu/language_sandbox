19188e4c0b6e741a0c0245e77a5b1a3d129346b4
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-04-13 00:58:33 +0900

    add ray test

--- rust/raytracing/src/ray.rs
+++ rust/raytracing/src/ray.rs
@@ -17,3 +17,18 @@ impl Ray {
         &self.origin + t * &self.direction
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use float_cmp::approx_eq;
+
+    #[test]
+    fn ray() {
+        let ray = Ray::new(Point3::new(1.0, 2.0, 3.0), Vec3::new(2.0, 2.0, 1.0));
+
+        approx_eq!(Point3, ray.at(0.0), Point3::new(1.0, 2.0, 3.0));
+
+        approx_eq!(Point3, ray.at(1.0), Point3::new(3.0, 4.0, 6.0));
+    }
+}
