98e33a04c932c7c22025dc6c89da43837ea0fe1e
curekoshimizu <shugo@manabe.mail-box.ne.jp>
2021-04-13 00:58:26 +0900

    refactor

--- rust/raytracing/src/color.rs
+++ rust/raytracing/src/color.rs
@@ -1,9 +1,9 @@
 use crate::vec3::Vec3;
-use float_cmp::approx_eq;
+use float_cmp::{approx_eq, ApproxEq, F64Margin};
 use std::fmt;
 use std::ops::Deref;
 
-struct Color(Vec3);
+pub struct Color(Vec3);
 
 impl Deref for Color {
     type Target = Vec3;
@@ -19,12 +19,27 @@ impl Color {
     }
 }
 
+impl From<Vec3> for Color {
+    fn from(v: Vec3) -> Self {
+        Color(v)
+    }
+}
+
 impl fmt::Display for Color {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let x = (255.999 * self.x) as u64;
-        let y = (255.999 * self.y) as u64;
-        let z = (255.999 * self.z) as u64;
+        let x = (255.999 * self.0.x) as u64;
+        let y = (255.999 * self.0.y) as u64;
+        let z = (255.999 * self.0.z) as u64;
         write!(f, "{} {} {}", x, y, z)
+    }
+}
+
+impl ApproxEq for Color {
+    type Margin = F64Margin;
+    fn approx_eq<M: Into<Self::Margin>>(self, other: Color, margin: M) -> bool {
+        let margin = margin.into();
+
+        self.0.approx_eq(other.0, margin)
     }
 }
 
@@ -32,17 +47,16 @@ impl fmt::Display for Color {
 mod tests {
     use super::*;
 
-    // #[test]
-    // fn add() {
-    //     let a = Color::new(1.0, 1.0, 1.0);
-    //     let b = Color::new(0.0, 1.0, 2.0);
-    //     // approx_eq!(Vec3, &a + &b, Color::new(1.0, 2.0, 3.0));
-    //     approx_eq!(Vec3, a + b, Color::new(1.0, 2.0, 3.0));
-    // }
+    #[test]
+    fn add() {
+        let a = Color::new(1.0, 1.0, 1.0);
+        let b = Color::new(0.0, 1.0, 2.0);
+        approx_eq!(Color, a.add(&b).into(), Color::new(1.0, 2.0, 3.0));
+    }
 
     #[test]
     fn display() {
         let color = Color::new(0.0, 0.5, 1.0);
-        assert_eq!(format!("Color {}", color), "Color 0 127 255");
+        assert_eq!(format!("Color({})", color), "Color(0 127 255)");
     }
 }
