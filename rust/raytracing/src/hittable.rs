3cc1967af4d8c36dac4b6765d669dd15680fadd8
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-04-13 00:58:38 +0900

    reutrn HitInfo

--- rust/raytracing/src/hittable.rs
+++ rust/raytracing/src/hittable.rs
@@ -1,5 +1,12 @@
 use crate::ray::Ray;
+use crate::vec3::{Point3, Vec3};
+
+pub struct HitInfo {
+    pub point: Point3,
+    pub outward_normal: Vec3,
+    pub t: f64,
+}
 
 pub trait Hittable {
-    fn hit(&mut self, ray: &Ray, t_min: f64, t_max: f64) -> bool;
+    fn hit(&mut self, ray: &Ray, t_min: f64, t_max: f64) -> Option<HitInfo>;
 }
