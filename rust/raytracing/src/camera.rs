c9a2d64503c5d0d6414111cc26c5b67305f75072
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-04-13 00:58:40 +0900

    add camera struct

---
+++ rust/raytracing/src/camera.rs
@@ -0,0 +1,37 @@
+use crate::ray::Ray;
+use crate::vec3::{Point3, Vec3};
+
+pub struct Camera {
+    origin: Point3,
+    horizontal: Vec3,
+    vertical: Vec3,
+    lower_left_cornerl: Vec3,
+}
+
+impl Camera {
+    pub fn new(aspect_raio: f64) -> Self {
+        let viewpoint_height: f64 = 2.0;
+        let viewpoint_width: f64 = aspect_raio * viewpoint_height;
+        let focal_length: f64 = 1.0;
+
+        let origin = Point3::new(0.0, 0.0, 0.0);
+        let horizontal = Vec3::new(viewpoint_width, 0.0, 0.0);
+        let vertical = Vec3::new(0.0, viewpoint_height, 0.0);
+        let lower_left_cornerl =
+            &origin - &horizontal / 2.0 - &vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
+
+        Camera {
+            origin: origin,
+            horizontal: horizontal,
+            vertical: vertical,
+            lower_left_cornerl: lower_left_cornerl,
+        }
+    }
+
+    pub fn get_ray(&self, u: f64, v: f64) -> Ray {
+        Ray::new(
+            self.origin.clone(),
+            &self.lower_left_cornerl + u * &self.horizontal + v * &self.vertical - &self.origin,
+        )
+    }
+}
