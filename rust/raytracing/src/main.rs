427407c8c56b568670ed8cee32dd0d6735bfc033
curekoshimizu <shugo@manabe.mail-box.ne.jp>
2021-04-13 00:58:44 +0900

    add rand mod

--- rust/raytracing/src/main.rs
+++ rust/raytracing/src/main.rs
@@ -1,6 +1,7 @@
 mod camera;
 mod color;
 mod hittable;
+mod rand;
 mod ray;
 mod sphere;
 mod vec3;
@@ -9,16 +10,12 @@ mod world;
 use crate::hittable::Hittable;
 use camera::Camera;
 use color::Color;
-use rand::distributions::{Distribution, Uniform};
-use rand::thread_rng;
 use ray::Ray;
 use sphere::Sphere;
 use std::fs::File;
 use std::io;
 use std::io::BufWriter;
 use std::io::Write;
-use std::sync::mpsc;
-use std::thread;
 use vec3::{Point3, Vec3};
 use world::World;
 
@@ -45,18 +42,7 @@ const IMAGE_HEIGHT: usize = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as usize;
 const SAMPLES_PER_PIXEL: usize = 100;
 
 fn main() -> io::Result<()> {
-    let (rand_tx, rand_rx) = mpsc::channel();
-
-    let handle = thread::spawn(move || {
-        // random number generator thread
-        let uniform_gen = Uniform::from(0.0..1.0);
-        let mut rng = thread_rng();
-        let rand_val = uniform_gen.sample(&mut rng);
-
-        for _ in 0..IMAGE_HEIGHT * IMAGE_WIDTH * SAMPLES_PER_PIXEL * 2 {
-            rand_tx.send(rand_val).unwrap();
-        }
-    });
+    let rand_uniform = rand::RandUniform::new(IMAGE_HEIGHT * IMAGE_WIDTH * SAMPLES_PER_PIXEL * 2);
 
     let camera = Camera::new(ASPECT_RATIO);
 
@@ -76,23 +62,20 @@ fn main() -> io::Result<()> {
             let mut pixel_color = Vec3::new(0.0, 0.0, 0.0);
 
             for _ in 0..SAMPLES_PER_PIXEL {
-                let u_delta = rand_rx.recv().unwrap();
-                let v_delta = rand_rx.recv().unwrap();
+                let u_delta = rand_uniform.gen();
+                let v_delta = rand_uniform.gen();
 
                 let u = (i as f64 + u_delta) / (IMAGE_WIDTH - 1) as f64;
                 let v = (j as f64 + v_delta) / (IMAGE_HEIGHT - 1) as f64;
                 let r = camera.get_ray(u, v);
 
                 let c: Vec3 = ray_color(&r, &mut world).into();
-
                 pixel_color += c;
             }
             pixel_color /= SAMPLES_PER_PIXEL as f64;
             write!(f, "{}\n", Color::from(pixel_color))?;
         }
     }
-
-    handle.join().unwrap();
 
     Ok(())
 }
