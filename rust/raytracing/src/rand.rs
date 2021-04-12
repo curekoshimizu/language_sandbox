5f7c2a553e886630e0be2c3a879350d60de09dd8
curekoshimizu <shugo@manabe.mail-box.ne.jp>
2021-04-13 00:58:56 +0900

    add ball uniform

--- rust/raytracing/src/rand.rs
+++ rust/raytracing/src/rand.rs
@@ -20,6 +20,27 @@ impl RandUniform {
     }
 }
 
+pub struct BallUniform(RandUniform);
+
+impl BallUniform {
+    pub fn new() -> Self {
+        BallUniform(RandUniform::new())
+    }
+
+    pub fn gen(&mut self) -> (f64, f64, f64) {
+        let cos_theta = -2.0 * self.0.gen() + 1.0;
+        let sin_theta = (1.0 - cos_theta * cos_theta).sqrt();
+        let phi = 2.0 * std::f64::consts::PI * self.0.gen();
+        let radius = self.0.gen().powf(1.0 / 3.0);
+
+        (
+            radius * sin_theta * phi.cos(),
+            radius * sin_theta * phi.sin(),
+            radius * cos_theta,
+        )
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -32,6 +53,19 @@ mod tests {
             let y = uniform.gen();
             assert!(0.0 <= x && x <= 1.0);
             assert!(0.0 <= y && y <= 1.0);
+            assert_ne!(x, y);
+        }
+    }
+
+    #[test]
+    fn rand_ball() {
+        {
+            let mut uniform = BallUniform::new();
+            let (x, y, z) = uniform.gen();
+            assert!(-1.0 <= x && x <= 1.0);
+            assert!(-1.0 <= y && y <= 1.0);
+            assert!(-1.0 <= z && z <= 1.0);
+            assert!(x * x + y * y + z * z <= 1.0);
             assert_ne!(x, y);
         }
     }
