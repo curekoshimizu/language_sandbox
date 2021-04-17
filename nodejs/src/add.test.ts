b570f7c13648f14adf2b4d4fb02f9e72971ae459
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-04-17 22:31:17 +0900

    support import for jest

--- nodejs/src/add.test.ts
+++ nodejs/src/add.test.ts
@@ -1,9 +1,5 @@
-// import add from './add';
-//
-// test('add', () => {
-//   expect(add(2, 3)).toBe(5);
-// });
+import add from './add';
 
 test('add', () => {
-  expect(2 + 3).toBe(5);
+  expect(add(2, 3)).toBe(5);
 });
