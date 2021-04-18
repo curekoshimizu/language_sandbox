8302044014bffe3223fe744072ba745d721c88aa
curekoshimizu <shugo@manabe.mail-box.ne.jp>
2021-04-18 23:22:31 +0900

    yet another sleep implement

--- nodejs/src/sleep.ts
+++ nodejs/src/sleep.ts
@@ -1,2 +1,6 @@
+import util from 'util';
+
 const sleep = (msec: number):Promise<void> => new Promise((resolve) => setTimeout(resolve, msec));
 export default sleep;
+
+export const sleep2 = util.promisify(setTimeout);
