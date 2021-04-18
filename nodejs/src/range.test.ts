8ef463c85d8c46c3daed3686a1e78aad78bd2dca
curekoshimizu <shugo@manabe.mail-box.ne.jp>
2021-04-18 15:26:42 +0900

    use describe and it instead of test

--- nodejs/src/range.test.ts
+++ nodejs/src/range.test.ts
@@ -1,29 +1,31 @@
 import range, { counter } from './range';
 
-test('range', () => {
-  const array = range(5);
-  expect(array.length).toBe(5);
+describe('range test', () => {
+  test('range', () => {
+    const array = range(5);
+    expect(array.length).toBe(5);
 
-  const squared = array.map((x) => x * x);
+    const squared = array.map((x) => x * x);
 
-  squared.forEach((x, i) => {
-    expect(i * i).toBe(x);
+    squared.forEach((x, i) => {
+      expect(i * i).toBe(x);
+    });
   });
-});
 
-test('count', () => {
-  const gen = counter();
-  expect(gen.next().value).toBe(0);
-  expect(gen.next().value).toBe(1);
-  expect(gen.next().value).toBe(2);
-  expect(gen.next().value).toBe(3);
+  test('count', () => {
+    const gen = counter();
+    expect(gen.next().value).toBe(0);
+    expect(gen.next().value).toBe(1);
+    expect(gen.next().value).toBe(2);
+    expect(gen.next().value).toBe(3);
 
-  let sum = 0;
-  for (const x of counter()) {
-    if (x > 3) {
-      break;
+    let sum = 0;
+    for (const x of counter()) {
+      if (x > 3) {
+        break;
+      }
+      sum += x;
     }
-    sum += x;
-  }
-  expect(sum).toBe(6);
+    expect(sum).toBe(6);
+  });
 });
--- nodejs/src/sleep.test.ts
+++ nodejs/src/sleep.test.ts
@@ -1,48 +1,50 @@
 import sleep from './sleep';
 
-test('sleep / date', async () => {
-  const startTime = new Date().getTime();
-  await sleep(100);
-  const elapsedTime = new Date().getTime() - startTime;
-  expect(elapsedTime).toBeGreaterThanOrEqual(99);
-  expect(elapsedTime).toBeLessThan(120);
-});
+describe('sleep test', () => {
+  it('sleep / date', async () => {
+    const startTime = new Date().getTime();
+    await sleep(100);
+    const elapsedTime = new Date().getTime() - startTime;
+    expect(elapsedTime).toBeGreaterThanOrEqual(99);
+    expect(elapsedTime).toBeLessThan(120);
+  });
 
-test('sleep / nodejs elapsed time', async () => {
-  const startTime = process.hrtime();
-  await sleep(50);
-  const elapsedTime = process.hrtime(startTime); // [sec, nano]
-  expect(elapsedTime[0]).toBe(0);
-  expect(elapsedTime[1]).toBeGreaterThan(50 * 1000000);
-  expect(elapsedTime[1]).toBeLessThan(52 * 1000000);
-});
+  it('sleep / nodejs elapsed time', async () => {
+    const startTime = process.hrtime();
+    await sleep(50);
+    const elapsedTime = process.hrtime(startTime); // [sec, nano]
+    expect(elapsedTime[0]).toBe(0);
+    expect(elapsedTime[1]).toBeGreaterThan(50 * 1000000);
+    expect(elapsedTime[1]).toBeLessThan(52 * 1000000);
+  });
 
-const lazyAdd = async (x: number, y: number, msec: number = 10):Promise<number> => {
-  await sleep(msec);
+  const lazyAdd = async (x: number, y: number, msec: number = 10):Promise<number> => {
+    await sleep(msec);
 
-  return x + y;
-};
+    return x + y;
+  };
 
-test('async gather', async () => {
-  const [x, y] = await Promise.all([lazyAdd(1, 1, 10), lazyAdd(1, 2)]);
-  const ret = await lazyAdd(x, y);
-  expect(ret).toBe(5);
-});
+  it('async gather', async () => {
+    const [x, y] = await Promise.all([lazyAdd(1, 1, 10), lazyAdd(1, 2)]);
+    const ret = await lazyAdd(x, y);
+    expect(ret).toBe(5);
+  });
 
-/* eslint func-style: off */
-async function* genNumbers(): AsyncGenerator<number> {
-  await sleep(10);
-  yield 1;
-  await sleep(10);
-  yield 2;
-  await sleep(10);
-  yield 3;
-}
-
-test('async generator', async () => {
-  let sum = 0;
-  for await (const x of genNumbers()) {
-    sum += x;
+  /* eslint func-style: off */
+  async function* genNumbers(): AsyncGenerator<number> {
+    await sleep(10);
+    yield 1;
+    await sleep(10);
+    yield 2;
+    await sleep(10);
+    yield 3;
   }
-  expect(sum).toBe(6);
+
+  it('async generator', async () => {
+    let sum = 0;
+    for await (const x of genNumbers()) {
+      sum += x;
+    }
+    expect(sum).toBe(6);
+  });
 });
