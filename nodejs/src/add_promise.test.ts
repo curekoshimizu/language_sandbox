7ccb81186751320641a1c6d53eb0cbbc499ffea9
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-04-18 15:26:42 +0900

    use describe and it instead of test

--- nodejs/src/add_promise.test.ts
+++ nodejs/src/add_promise.test.ts
@@ -2,41 +2,43 @@ import add, { addWithCallback, addPromise, addPromiseFromCallback } from './add_
 
 const ans = 5;
 
-test('add', () => {
-  expect(add(2, 3)).toBe(ans);
-});
+describe('promise / async test', () => {
+  test('add', () => {
+    expect(add(2, 3)).toBe(ans);
+  });
 
-test('add callback', (done) => {
-  const verify = (ret: number):void => {
+  test('add callback', (done) => {
+    const verify = (ret: number):void => {
+      expect(ret).toBe(ans);
+      done();
+    };
+
+    addWithCallback(2, 3, verify);
+  });
+
+  test('add promise', () => {
+    const verify = (ret: number):void => {
+      expect(ret).toBe(ans);
+    };
+
+    return addPromise(2, 3).then(verify);
+  });
+
+  test('add promise 2', () => {
+    const verify = (ret: number):void => {
+      expect(ret).toBe(ans);
+    };
+
+    return addPromiseFromCallback(2, 3).then(verify);
+  });
+
+  test('add async', async () => {
+    const ret = await addPromise(2, 3);
     expect(ret).toBe(ans);
-    done();
-  };
+  });
 
-  addWithCallback(2, 3, verify);
-});
-
-test('add promise', () => {
-  const verify = (ret: number):void => {
+  test('add async 2', async () => {
+    const ret = await addPromiseFromCallback(2, 3);
     expect(ret).toBe(ans);
-  };
-
-  return addPromise(2, 3).then(verify);
-});
-
-test('add promise 2', () => {
-  const verify = (ret: number):void => {
-    expect(ret).toBe(ans);
-  };
-
-  return addPromiseFromCallback(2, 3).then(verify);
-});
-
-test('add async', async () => {
-  const ret = await addPromise(2, 3);
-  expect(ret).toBe(ans);
-});
-
-test('add async 2', async () => {
-  const ret = await addPromiseFromCallback(2, 3);
-  expect(ret).toBe(ans);
+  });
 });
