90664eebc8d19d628bc23212af962ef83bf29fb8
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-04-18 23:38:14 +0900

    write/read example

--- nodejs/src/fs_temp.test.ts
+++ nodejs/src/fs_temp.test.ts
@@ -1,5 +1,7 @@
 import { using } from 'using-statement';
+import path from 'path';
 import fs from 'fs';
+import readline from 'readline';
 import TempDir, { AsyncTempDir } from './fs_temp';
 
 describe('temp', () => {
@@ -16,5 +18,62 @@ describe('temp', () => {
       expect(fs.existsSync(temp.tempDir())).toBeTruthy();
     });
     expect(fs.existsSync(temp.tempDir())).toBeFalsy();
+  });
+});
+
+describe('write data only once then read (string data)', () => {
+  it('sync', () => {
+    const temp = new TempDir();
+    using(temp, () => {
+      const fname = path.join(temp.tempDir(), 'file.txt');
+      const data = 'party parrot';
+      fs.writeFileSync(fname, data);
+      const readData = fs.readFileSync(fname, 'utf-8');
+      expect(readData).toBe(data);
+    });
+  });
+  it('async', async () => {
+    const temp = await AsyncTempDir.new();
+    await using(temp, async () => {
+      const fname = path.join(temp.tempDir(), 'file.txt');
+      const data = 'party parrot';
+      await fs.promises.writeFile(fname, data);
+      const readData = await fs.promises.readFile(fname, 'utf-8');
+      expect(readData).toBe(data);
+    });
+  });
+});
+
+describe('write data 10000 times then read', () => {
+  it('async string', async () => {
+    const temp = await AsyncTempDir.new();
+    const count = 10000;
+
+    await using(temp, async () => {
+      const fname = path.join(temp.tempDir(), 'file.txt');
+      const data = 'party parrot';
+
+      await new Promise<void>((resolve) => {
+        const stream = fs.createWriteStream(fname);
+        stream.on('finish', () => {
+          resolve();
+        });
+        [...Array(count)].forEach(() => {
+          stream.write(`${data}\n`);
+        });
+        stream.end(); // to flush
+      });
+
+      const readStream = fs.createReadStream(fname, { encoding: 'utf8' });
+      const reader = readline.createInterface({ input: readStream });
+
+      let cnt = 0;
+      for await (const line of reader) {
+        expect(line).toBe(data);
+        cnt += 1;
+      }
+
+      expect(cnt).toBe(count);
+    });
   });
 });
