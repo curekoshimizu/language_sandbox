748b25b3dab1d0c4c5f8ea6ee5a3202d304d89f1
curekoshimizu <shugo@manabe.mail-box.ne.jp>
2021-05-30 16:32:56 +0900

    add StyledButton

--- react/todo_apps/src/App.tsx
+++ react/todo_apps/src/App.tsx
@@ -1,4 +1,6 @@
 import { createMuiTheme } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import {
   ThemeProvider as MaterialThemeProvider,
   StylesProvider,
@@ -6,6 +8,13 @@ import {
 import styled, {
   ThemeProvider as StyledThemeProvider,
 } from 'styled-components';
+
+const StyledButton = styled(Button)`
+  font-size: 2em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border-radius: 3px;
+`;
 
 const theme = createMuiTheme({
   palette: {
@@ -28,7 +37,11 @@ const App: React.FC = () => (
   <StylesProvider injectFirst>
     <MaterialThemeProvider theme={theme}>
       <StyledThemeProvider theme={theme}>
-        <div>Hello</div>;
+        <CssBaseline />
+        <StyledButton color="primary" variant="contained">
+          Default
+        </StyledButton>
+        <div>Hello</div>
       </StyledThemeProvider>
     </MaterialThemeProvider>
   </StylesProvider>
