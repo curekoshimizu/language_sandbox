7a1dd8bb39794cdc9992e0c557fe1e6262bb0264
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-05-30 20:54:08 +0900

    Buttons

---
+++ react/todo_apps/src/apps/Button.tsx
@@ -0,0 +1,34 @@
+import { Button } from '@material-ui/core';
+import { ButtonTypeMap } from '@material-ui/core/Button';
+
+export default (() => {
+  const variants: Array<ButtonTypeMap['props']['variant']> = [
+    'contained',
+    'outlined',
+    'text',
+  ];
+  return (
+    <>
+      {variants.map((variant) => (
+        <div>
+          <Button variant={variant}>Default</Button>
+          <Button color="primary" variant={variant}>
+            primary
+          </Button>
+          <Button color="secondary" variant={variant}>
+            secondary
+          </Button>
+          <Button disabled variant={variant}>
+            disabled
+          </Button>
+          <Button color="primary" disabled variant={variant}>
+            disabled primary
+          </Button>
+          <Button color="secondary" disabled variant={variant}>
+            disabled secondary
+          </Button>
+        </div>
+      ))}
+    </>
+  );
+}) as React.FC;
