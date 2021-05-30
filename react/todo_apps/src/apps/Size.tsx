df94031785e8b89a75519fc49361c7b3ba098a26
curekoshimizu <shugo@manabe.mail-box.ne.jp>
2021-05-31 02:33:45 +0900

    alignment page

--- react/todo_apps/src/apps/Size.tsx
+++ react/todo_apps/src/apps/Size.tsx
@@ -1,4 +1,54 @@
 import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+
+const Alignment = () => {
+  return (
+    <>
+      <Typography component="h2" gutterBottom variant="h3">
+        Alignment
+      </Typography>
+
+      <Box
+        alignItems="center"
+        bgcolor="lightblue"
+        display="flex"
+        height={130}
+        justifyContent="center"
+        width={500}
+      >
+        <Box border={1} height={40} width={100}>
+          Linux
+        </Box>
+        <Box border={1} height={40} width={100}>
+          Windows
+        </Box>
+        <Box border={1} height={40} width={100}>
+          Mac
+        </Box>
+      </Box>
+      <Box
+        alignItems="center"
+        bgcolor="lightgreen"
+        display="flex"
+        height={80}
+        justifyContent="center"
+        width={500}
+      >
+        2. lightgreen
+      </Box>
+      <Box
+        alignItems="center"
+        bgcolor="pink"
+        display="flex"
+        height={80}
+        justifyContent="center"
+        width={500}
+      >
+        3. pink
+      </Box>
+    </>
+  );
+};
 
 export default (() => {
   return (
@@ -21,6 +71,7 @@ export default (() => {
       <Box bgcolor="grey.400" my={0.5} p={1} width={1}>
         Width 1
       </Box>
+      <Alignment />
     </Box>
   );
 }) as React.FC;
