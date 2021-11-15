e40bb7d045154e63e1f5284686d0179808c50a4f
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2021-11-15 21:11:29 +0900

    implement Async Await

--- golang/async_await/async_await_test.go
+++ golang/async_await/async_await_test.go
@@ -2,12 +2,30 @@ package async_await
 
 import (
     "testing"
+    "time"
 
     "github.com/stretchr/testify/assert"
 )
 
 
-func TestAdd(t *testing.T) {
-    assert.Equal(t, Add(1, 2), 3);
+func BlockSleep(msec int) int {
+	time.Sleep( time.Duration(msec) * time.Millisecond)
+	return msec
 }
 
+func TestAsyncAwait(t *testing.T) {
+    now:=time.Now()
+
+
+    msec := 100
+	future := Exec(func() interface{} {
+		return BlockSleep(msec)
+	})
+	val := future.Await().(int)
+    assert.Equal(t, val, msec)
+
+    ans := time.Since(now).Milliseconds()
+    expect := int64(msec) *90/100 // 90%
+    assert.True(t, ans > expect, "expect :  %v > %v", ans, expect)
+
+}
