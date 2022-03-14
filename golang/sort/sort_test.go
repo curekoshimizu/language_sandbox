f6785626392c0dc6b3bad7bb45caf9243d23ea4a
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2022-03-14 13:07:47 +0900

    add sort test

---
+++ golang/sort/sort_test.go
@@ -0,0 +1,67 @@
+package sort_test
+
+import (
+	"math/rand"
+	"sort"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func randomDate() time.Time {
+	min := time.Date(2000, 1, 0, 0, 0, 0, 0, time.UTC).Unix()
+	max := time.Date(2022, 1, 0, 0, 0, 0, 0, time.UTC).Unix()
+	delta := max - min
+
+	sec := rand.Int63n(delta) + min
+	return time.Unix(sec, 0)
+}
+
+func TestSortTime(t *testing.T) {
+	length := 100
+	slice := make([]time.Time, 0, length)
+	for i := 0; i < length; i++ {
+		slice = append(slice, randomDate())
+	}
+
+	// sort
+	sort.Slice(slice, func(i, j int) bool {
+		return slice[i].Before(slice[j])
+	})
+
+	// check
+	assert.True(t, slice[0].Before(slice[length-1]))
+}
+
+type Tuple struct {
+	X int
+	Y float64
+}
+
+func randomTuple() Tuple {
+	return Tuple{
+		X: rand.Int(),
+		Y: float64(rand.Int()),
+	}
+}
+
+func TestStructSort(t *testing.T) {
+	length := 100
+	slice := make([]Tuple, 0, length)
+	for i := 0; i < length; i++ {
+		slice = append(slice, randomTuple())
+	}
+
+	satisfiedFunc := func(i, j int) bool {
+		if slice[i].X == slice[j].X {
+			return slice[i].Y < slice[j].Y
+		}
+		return slice[i].X < slice[j].X
+	}
+	// sort
+	sort.Slice(slice, satisfiedFunc)
+
+	// check
+	assert.True(t, satisfiedFunc(0, length-1))
+}
