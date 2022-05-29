3d22011d5e2eddffcbf1fbb5b8b3e4572ea18643
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2022-05-29 17:00:41 +0900

    implement iris_prediction

--- pytorch_sandbox/pytorch_sandbox/test_iris.py
+++ pytorch_sandbox/pytorch_sandbox/test_iris.py
@@ -17,19 +17,17 @@ class Net(nn.Module):
         return ret
 
 
-def test_iris_prediction(save: bool = False) -> None:
-    torch.manual_seed(123)
-
+def test_iris_two_class_prediction(save: bool = False) -> None:
     iris = load_iris()
-    x_orgin = iris.data
+    x_origin = iris.data
     y_origin = iris.target
 
-    assert x_orgin.shape == (150, 4)
+    assert x_origin.shape == (150, 4)
     assert y_origin.shape == (150,)
 
     # choose only class0 or class1 data
     indexes = (y_origin == 0) | (y_origin == 1)
-    x_data = x_orgin[indexes, :2]
+    x_data = x_origin[indexes, :2]
     y_data = y_origin[indexes]
     assert np.count_nonzero((y_data == 0) | (y_data == 1)) == 100
 
@@ -38,6 +36,7 @@ def test_iris_prediction(save: bool = False) -> None:
         y_data,
         train_size=70,
         test_size=30,
+        random_state=123,
     )
 
     net = Net()
@@ -118,3 +117,99 @@ def test_iris_prediction(save: bool = False) -> None:
         figure3.plot(history_array[:, 0], history_array[:, 4], color="blue")  # test acc
 
         figure.savefig("sepal.png")
+
+
+class IrisNet(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.l1 = nn.Linear(in_features=2, out_features=3)
+
+        self.l1.weight.data.fill_(1.0)
+        self.l1.bias.data.fill_(1.0)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        ret = self.l1(x)
+        assert isinstance(ret, torch.Tensor)
+        return ret
+
+
+def test_iris_prediction(save: bool = False) -> None:
+    iris = load_iris()
+    x_origin = iris.data
+    y_origin = iris.target
+
+    assert x_origin.shape == (150, 4)
+    assert y_origin.shape == (150,)
+
+    x_train, x_test, y_train, y_test = train_test_split(
+        x_origin,
+        y_origin,
+        train_size=75,
+        test_size=75,
+        random_state=123,
+    )
+
+    net = nn.Linear(in_features=4, out_features=3)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.01)
+
+    inputs = torch.tensor(x_train).float()
+    labels = torch.tensor(y_train).long()
+    assert inputs.shape == (75, 4)
+    assert labels.shape == (75,)  # for CrossEntropyLoss. not (75, 1)!!
+
+    inputs_test = torch.tensor(x_test).float()
+    labels_test = torch.tensor(y_test).long()
+    assert inputs_test.shape == (75, 4)
+    assert labels_test.shape == (75,)
+
+    history = []
+    prev_loss = 0.0
+    num_epochs = 10000
+    for epoch in range(num_epochs):
+        # train
+        optimizer.zero_grad()
+        outputs = net(inputs)
+
+        assert outputs.shape == (75, 3)
+        assert labels.shape == (75,)
+
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        _, predicted_label = torch.max(outputs, dim=1)  # max returns (values, indices)
+        assert len(predicted_label) == 75
+        acc_train = ((predicted_label == labels).sum() / len(labels)).item()
+
+        # test
+        outputs_test = net(inputs_test)
+        loss_test = criterion(outputs_test, labels_test).item()
+        _, predicted_label_test = torch.max(outputs_test, dim=1)  # max returns (values, indices)
+        acc_test = ((predicted_label_test == labels_test).sum() / len(labels_test)).item()
+
+        assert isinstance(loss, torch.Tensor) and loss.ndim == 0
+        # that's why we need to call .item()
+        current_loss = loss.item()
+        if epoch % 100 == 0:
+            print(
+                "epoch : ", epoch, "loss", current_loss, "acc", acc_train, "loss_test", loss_test, "acc_test", acc_test
+            )
+            if abs(prev_loss - current_loss) < 1.0e-7:
+                break
+            prev_loss = current_loss
+        history.append((epoch, current_loss, acc_train, loss_test, acc_test))
+
+    if save:
+        figure = plt.figure()
+        figure1 = figure.add_subplot(2, 1, 1)
+        history_array = np.array(history)
+        figure1.plot(history_array[:, 0], history_array[:, 1], color="green")  # train loss
+        figure1.plot(history_array[:, 0], history_array[:, 3], color="blue")  # test loss
+
+        figure2 = figure.add_subplot(2, 1, 2)
+        history_array = np.array(history)
+        figure2.plot(history_array[:, 0], history_array[:, 2], color="green")  # train acc
+        figure2.plot(history_array[:, 0], history_array[:, 4], color="blue")  # test acc
+
+        figure.savefig("iris.png")
