324d3e5f7eb9029adb0d8a8f6db7753db25282ff
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2022-06-05 13:45:20 +0900

    add transform

--- pytorch_sandbox/pytorch_sandbox/test_janken.py
+++ pytorch_sandbox/pytorch_sandbox/test_janken.py
@@ -1,11 +1,13 @@
 import enum
 import pathlib
+from typing import Callable, Optional
 
 import matplotlib.pyplot as plt
 import torch
 # import numpy as np
 from PIL import Image
 from torch.utils.data import Dataset, random_split
+from torchvision import transforms
 
 script_path = pathlib.Path(__file__).parent.resolve()
 
@@ -17,7 +19,11 @@ class Janken(enum.Enum):
 
 
 class JankenDataset(Dataset[tuple[Image.Image, Janken]]):
-    def __init__(self) -> None:
+    def __init__(
+        self,
+        input_size: Optional[tuple[int, int]] = None,
+        transform: Optional[Callable[[Image.Image], torch.Tensor]] = None,
+    ) -> None:
         super().__init__()
         root_dir = script_path.parents[0] / "datasets" / "janken_dataset"
         gu = root_dir / "gu"
@@ -31,19 +37,28 @@ class JankenDataset(Dataset[tuple[Image.Image, Janken]]):
             data.append((d, Janken.CHOKI))
         for d in pa.glob("*.JPG"):
             data.append((d, Janken.PA))
+        if input_size is None:
+            input_size = (64, 64)
+        if transform is None:
+            transform = transforms.Compose(
+                [
+                    transforms.Resize(input_size),
+                    transforms.ToTensor(),
+                ]
+            )
+        self._transform = transform
         self._data = data
 
     def __len__(self) -> int:
         return len(self._data)
 
-    def __getitem__(self, index: int) -> tuple[Image.Image, Janken]:
+    def __getitem__(self, index: int) -> tuple[torch.Tensor, Janken]:
         assert 0 <= index < len(self)
         image_path, label = self._data[index]
 
         image = Image.open(image_path)
-        # image = image.resize(self.input_size)  # リサイズ
-        # image = np.array(image).astype(np.float32).transpose(2, 1, 0)  # Dataloader で使うために転置する
-
+        image = self._transform(image)
+        assert isinstance(image, torch.Tensor)
         return image, label
 
 
@@ -64,6 +79,10 @@ def test_dataloader(seed: int = 100) -> None:
 def test_save_image() -> None:
     dataset = JankenDataset()
     img, gu = dataset[0]
+    assert img.shape == (3, 64, 64)
     assert gu == Janken.GU
-    plt.imshow(img)
+    # How to display torch.Tensor
+    # https://stackoverflow.com/questions/53623472/how-do-i-display-a-single-image-in-pytorch
+    assert img.permute(1, 2, 0).shape == (64, 64, 3)
+    plt.imshow(img.permute(1, 2, 0))
     plt.savefig("gu.png")
